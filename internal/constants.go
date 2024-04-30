d3b7c246fa6317099bdb586aa9d135032767831a
Josh Guarino <jguarino722@gmail.com>
2024-04-30 14:28:51 -0400

    playing around with ideas

---
+++ internal/constants.go
@@ -0,0 +1,8 @@
+package internal
+
+const (
+	PokeApiBaseUrl = "https://pokeapi.co/api/v2"
+	Berry          = PokeApiBaseUrl + "/berry"
+	BerryFirmness  = PokeApiBaseUrl + "/berry-firmness"
+	BerryFlavor    = PokeApiBaseUrl + "/berry-flavor"
+)

fc2e72fff24d13048fb2a46eb9e8269c5dd387db
Josh Guarino <jguarino722@gmail.com>
2024-05-02 15:56:54 -0400

    added evolution resources

---
+++ internal/evolution/constants.go
@@ -0,0 +1,8 @@
+package evolution
+
+import "github.com/joshguarino/poketool/internal"
+
+const (
+	evolutionChainEndpoint   = internal.PokeApiBaseUrl + "/evolution-chain"
+	evolutionTriggerEndpoint = internal.PokeApiBaseUrl + "/evolution-trigger"
+)
--- internal/evolution/evolution.go
+++ internal/evolution/evolution.go
@@ -1,1 +1,38 @@
 package evolution
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joshguarino/poketool/internal"
+	"github.com/mtslzr/pokeapi-go"
+	"github.com/mtslzr/pokeapi-go/structs"
+)
+
+func GetEvolutionChain(nameOrId string) structs.EvolutionChain {
+	evolutionChain, err := pokeapi.EvolutionChain(nameOrId)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return evolutionChain
+}
+
+func GetEvolutionChainList() structs.Resource {
+	evolutionChainList := internal.GetResourceList(evolutionChainEndpoint)
+	return evolutionChainList
+}
+
+func GetEvolutionTrigger(nameOrId string) structs.EvolutionTrigger {
+	evolutionTrigger, err := pokeapi.EvolutionTrigger(nameOrId)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return evolutionTrigger
+}
+
+func GetEvolutionTriggerList() structs.Resource {
+	evolutionTriggerList := internal.GetResourceList(evolutionTriggerEndpoint)
+	return evolutionTriggerList
+}

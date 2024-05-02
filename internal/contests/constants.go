a683e280023491aa5a0dc2d350deea2dfc4a61dc
Josh Guarino <jguarino722@gmail.com>
2024-05-01 23:03:37 -0400

    added contests resources

---
+++ internal/contests/constants.go
@@ -0,0 +1,9 @@
+package contests
+
+import "github.com/joshguarino/poketool/internal"
+
+const (
+	contestTypeEndpoint        = internal.PokeApiBaseUrl + "contest-type"
+	contestEffectEndpoint      = internal.PokeApiBaseUrl + "/contest-effect"
+	superContestEffectEndpoint = internal.PokeApiBaseUrl + "/super-contest-effect"
+)
--- internal/contests/contests.go
+++ internal/contests/contests.go
@@ -1,1 +1,52 @@
 package contests
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joshguarino/poketool/internal"
+	"github.com/mtslzr/pokeapi-go"
+	"github.com/mtslzr/pokeapi-go/structs"
+)
+
+func GetContestType(nameOrId string) structs.ContestType {
+	contestType, err := pokeapi.ContestType(nameOrId)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return contestType
+}
+
+func GetContetTypeList() structs.Resource {
+	contestTypeList := internal.GetResourceList(contestTypeEndpoint)
+	return contestTypeList
+}
+
+func GetContestEffect(nameOrId string) structs.ContestEffect {
+	contestEffect, err := pokeapi.ContestEffect(nameOrId)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return contestEffect
+}
+
+func GetContestEffectList() structs.Resource {
+	contestEffectList := internal.GetResourceList(contestEffectEndpoint)
+	return contestEffectList
+}
+
+func GetSuperContestEffect(nameOrId string) structs.SuperContestEffect {
+	superContestEffect, err := pokeapi.SuperContestEffect(nameOrId)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return superContestEffect
+}
+
+func GetSuperContestEffectList() structs.Resource {
+	superContestEffectList := internal.GetResourceList(superContestEffectEndpoint)
+	return superContestEffectList
+}

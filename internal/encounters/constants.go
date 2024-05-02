663d19cd1d09f49819ea0bfd5015f7140888f7d5
Josh Guarino <jguarino722@gmail.com>
2024-05-02 13:04:09 -0400

    added encounters resources

---
+++ internal/encounters/constants.go
@@ -0,0 +1,9 @@
+package encounters
+
+import "github.com/joshguarino/poketool/internal"
+
+const (
+	encounterMethodEndpoint         = internal.PokeApiBaseUrl + "/encounter-method"
+	encounterConditionEndpoint      = internal.PokeApiBaseUrl + "/encounter-condition"
+	encounterConditionValueEndpoint = internal.PokeApiBaseUrl + "/encounter-condition-value"
+)
--- internal/encounters/encounters.go
+++ internal/encounters/encounters.go
@@ -1,1 +1,52 @@
 package encounters
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joshguarino/poketool/internal"
+	"github.com/mtslzr/pokeapi-go"
+	"github.com/mtslzr/pokeapi-go/structs"
+)
+
+func GetEncounterMethod(nameOrId string) structs.EncounterMethod {
+	encounterMethod, err := pokeapi.EncounterMethod(nameOrId)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return encounterMethod
+}
+
+func GetEncounterMethodList() structs.Resource {
+	encounterMethodList := internal.GetResourceList(encounterMethodEndpoint)
+	return encounterMethodList
+}
+
+func GetEncounterCondition(nameOrId string) structs.EncounterCondition {
+	encounterCondition, err := pokeapi.EncounterCondition(nameOrId)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return encounterCondition
+}
+
+func GetEncounterConditionList() structs.Resource {
+	encounterConditionList := internal.GetResourceList(encounterConditionEndpoint)
+	return encounterConditionList
+}
+
+func GetEncounterConditionValue(nameOrId string) structs.EncounterConditionValue {
+	encounterConditionValue, err := pokeapi.EncounterConditionValue(nameOrId)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return encounterConditionValue
+}
+
+func GetEncounterConditionValueList() structs.Resource {
+	encounterConditionValueList := internal.GetResourceList(encounterConditionValueEndpoint)
+	return encounterConditionValueList
+}

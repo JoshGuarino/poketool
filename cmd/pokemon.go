3aae40c374268325c0413532722c460541d423d4
Josh Guarino <jguarino722@gmail.com>
2024-05-20 18:06:30 -0400

    figuring out how cli will work

--- cmd/pokemon.go
+++ cmd/pokemon.go
@@ -11,41 +11,55 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var search bool
+
 // pokemonCmd represents the pokemon command
 var pokemonCmd = &cobra.Command{
 	Use:   "pokemon",
 	Short: "Access pokemon resource group data from pokeapi: https://pokeapi.co/docs/v2#pokemon-section",
-	// Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
+
+		// select prompt
 		prompt := promptui.Select{
 			Label: "Select pokemon group resource",
-			Items: []string{"abilities", "characteristics", "egg group", "gender", "growth rate", "nature",
-				"pokeathlon stat", "pokemon", "pokemon color", "pokemon form", "pokemon habitat", "pokemon shape",
-				"pokemon species", "stat", "type"},
+			Items: []string{"Abilities", "Characteristics", "Egg group", "Gender", "Growth Rate", "Nature",
+				"Pokeathlon Stat", "Pokemon", "Pokemon Color", "Pokemon Form", "Pokemon Habitat", "Pokemon Shape",
+				"Pokemon Species", "Stat", "Type"},
 		}
-
 		_, result, err := prompt.Run()
 		if err != nil {
 			fmt.Printf("Prompt failed %v\n", err)
 			return
 		}
-		fmt.Printf("You choose %q\n", result)
 
-		// fmt.Println(prompt)
-		pokemon.GetPokemonLocationAreas("1")
+		// flag to search for specific resource
+		if search {
+			// search prompt
+			prompt := promptui.Prompt{
+				Label: "Search",
+			}
+			result, err := prompt.Run()
+			if err != nil {
+				fmt.Printf("Prompt failed %v\n", err)
+				return
+			}
+
+			s := pokemon.GetSpecfic(result)
+			fmt.Println(s)
+			return
+		}
+
+		l := pokemon.GetList(result)
+		fmt.Println(l)
+
+		// p := pokemon.GetPokemon("1")
+		// s, _ := json.MarshalIndent(p, "", "\t")
+		// fmt.Printf("%+v\n", s)
+		// fmt.Println(string(s))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(pokemonCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	pokemonCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	pokemonCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	pokemonCmd.Flags().BoolVarP(&search, "search", "s", false, "Find specific resource")
 }
---
+++ internal/pokemon/controller.go
@@ -0,0 +1,75 @@
+package pokemon
+
+func GetList(result string) string {
+	switch result {
+	case "Abilities":
+		//fmt.Println()
+	case "Characteristics":
+		//fmt.Println()
+	case "Egg Group":
+		//fmt.Println()
+	case "Gender":
+		//fmt.Println()
+	case "Growth Rate":
+		//fmt.Println()
+	case "Nature":
+		//fmt.Println()
+	case "Pokeathlon Stat":
+		//fmt.Println()
+	case "Pokemon":
+		//fmt.Println()
+	case "Pokemon Color":
+		//fmt.Println()
+	case "Pokemon Form":
+		//fmt.Println()
+	case "pokemon Habitat":
+		//fmt.Println()
+	case "Pokemon Shape":
+		//fmt.Println()
+	case "Pokemon Species":
+		//fmt.Println()
+	case "Stat":
+		//fmt.Println()
+	case "Type":
+		//fmt.Println()
+	}
+
+	return "test"
+}
+
+func GetSpecfic(result string) string {
+	switch result {
+	case "Abilities":
+		//fmt.Println()
+	case "Characteristics":
+		//fmt.Println()
+	case "Egg Group":
+		//fmt.Println()
+	case "Gender":
+		//fmt.Println()
+	case "Growth Rate":
+		//fmt.Println()
+	case "Nature":
+		//fmt.Println()
+	case "Pokeathlon Stat":
+		//fmt.Println()
+	case "Pokemon":
+		//fmt.Println()
+	case "Pokemon Color":
+		//fmt.Println()
+	case "Pokemon Form":
+		//fmt.Println()
+	case "pokemon Habitat":
+		//fmt.Println()
+	case "Pokemon Shape":
+		//fmt.Println()
+	case "Pokemon Species":
+		//fmt.Println()
+	case "Stat":
+		//fmt.Println()
+	case "Type":
+		//fmt.Println()
+	}
+
+	return ""
+}

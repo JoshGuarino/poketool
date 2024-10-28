832a38f3919cfc81e35d0b6e36cb0a1029d09fa0
Josh Guarino <jguarino722@gmail.com>
2024-10-28 13:29:39 -0400

    fixed contest type endpoint constant

--- internal/contests/constants.go
+++ internal/contests/constants.go
@@ -3,7 +3,7 @@ package contests
 import "github.com/joshguarino/poketool/internal"
 
 const (
-	contestTypeEndpoint        = internal.PokeApiBaseUrl + "contest-type"
+	contestTypeEndpoint        = internal.PokeApiBaseUrl + "/contest-type"
 	contestEffectEndpoint      = internal.PokeApiBaseUrl + "/contest-effect"
 	superContestEffectEndpoint = internal.PokeApiBaseUrl + "/super-contest-effect"
 )

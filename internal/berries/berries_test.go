fb54a70b95e99e8863ccf870330ef139661a45f7
Josh Guarino <jguarino722@gmail.com>
2024-11-13 11:30:22 -0500

    implemented encoutners tests

--- internal/berries/berries_test.go
+++ internal/berries/berries_test.go
@@ -9,40 +9,40 @@ func TestGetBerry(t *testing.T) {
 	rById, _ := GetBerry("1")
 	rByName, _ := GetBerry("cheri")
 	rFail, _ := GetBerry("test")
-	assert.Equal(t, "cheri", rById.Name, "Expected to receive Cheri.")
-	assert.Equal(t, "cheri", rByName.Name, "Expected to receive Cheri.")
+	assert.Equal(t, "cheri", rById.Name, "Expected to receive 'cheri'.")
+	assert.Equal(t, "cheri", rByName.Name, "Expected to receive 'cheri'.")
 	assert.Equal(t, "", rFail.Name, "Expected to receive an empty result.")
 }
 
 func TestGetBerryList(t *testing.T) {
-	result := GetBerryList()
-	assert.Equal(t, "cheri", result.Results[0].Name, "Expected to have a list berries resource.")
+	r := GetBerryList()
+	assert.Equal(t, "cheri", r.Results[0].Name, "Expected to have a list of 'berry' resource.")
 }
 
 func TestGetBerryFirmness(t *testing.T) {
 	rById, _ := GetBerryFirmness("1")
 	rByName, _ := GetBerryFirmness("very-soft")
 	rFail, _ := GetBerryFirmness("test")
-	assert.Equal(t, "very-soft", rById.Name, "Expected to receive Very Soft.")
-	assert.Equal(t, "very-soft", rByName.Name, "Expected to receive Very Soft.")
+	assert.Equal(t, "very-soft", rById.Name, "Expected to receive 'very-soft'.")
+	assert.Equal(t, "very-soft", rByName.Name, "Expected to receive 'very-soft'.")
 	assert.Equal(t, "", rFail.Name, "Expected to receive an empty result.")
 }
 
 func TestGetBerryFirmnessList(t *testing.T) {
-	result := GetBerryFirmnessList()
-	assert.Equal(t, "very-soft", result.Results[0].Name, "Expected to have a list of berry firmness resource.")
+	r := GetBerryFirmnessList()
+	assert.Equal(t, "very-soft", r.Results[0].Name, "Expected to have a list of 'berry firmness' resource.")
 }
 
 func TestGetBerryFlavor(t *testing.T) {
 	rById, _ := GetBerryFlavor("1")
 	rByName, _ := GetBerryFlavor("spicy")
 	rFail, _ := GetBerryFlavor("test")
-	assert.Equal(t, "spicy", rById.Name, "Expected to receive Spicy.")
-	assert.Equal(t, "spicy", rByName.Name, "Expected to receive Spicy.")
+	assert.Equal(t, "spicy", rById.Name, "Expected to receive 'spicy'.")
+	assert.Equal(t, "spicy", rByName.Name, "Expected to receive 'spicy'.")
 	assert.Equal(t, "", rFail.Name, "Expected to receive an empty result.")
 }
 
 func TestGetBerryFlavorList(t *testing.T) {
-	result := GetBerryFlavorList()
-	assert.Equal(t, "spicy", result.Results[0].Name, "Expected to have a list of berry flavor resource.")
+	r := GetBerryFlavorList()
+	assert.Equal(t, "spicy", r.Results[0].Name, "Expected to have a list of 'berry flavor' resource.")
 }
--- internal/encounters/encounters_test.go
+++ internal/encounters/encounters_test.go
@@ -1,1 +1,48 @@
 package encounters
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetEncounterMethod(t *testing.T) {
+	rById, _ := GetEncounterMethod("1")
+	rByName, _ := GetEncounterMethod("walk")
+	rFail, _ := GetEncounterMethod("test")
+	assert.Equal(t, "walk", rById.Name, "Expected to receive 'walk'.")
+	assert.Equal(t, "walk", rByName.Name, "Expected to receive 'walk'.")
+	assert.Equal(t, "", rFail.Name, "Expected to receive an empty result.")
+}
+
+func TestGetEncounterMethodList(t *testing.T) {
+	r := GetEncounterMethodList()
+	assert.Equal(t, "walk", r.Results[0].Name, "Expected to have a list of 'encounter method' resource.")
+}
+
+func TestGetEncounterCondition(t *testing.T) {
+	rById, _ := GetEncounterCondition("1")
+	rByName, _ := GetEncounterCondition("swarm")
+	rFail, _ := GetEncounterCondition("test")
+	assert.Equal(t, "swarm", rById.Name, "Expected to receive 'swarm'.")
+	assert.Equal(t, "swarm", rByName.Name, "Expected to receive 'swarm'.")
+	assert.Equal(t, "", rFail.Name, "Expected to receive an empty result.")
+}
+
+func TestGetEncounterConditionList(t *testing.T) {
+	r := GetEncounterConditionList()
+	assert.Equal(t, "swarm", r.Results[0].Name, "Expected to have a list of 'encounter condition' resource.")
+}
+
+func TestGetEncounterConditionValue(t *testing.T) {
+	rById, _ := GetEncounterConditionValue("1")
+	rByName, _ := GetEncounterConditionValue("swarm-yes")
+	rFail, _ := GetEncounterConditionValue("test")
+	assert.Equal(t, "swarm-yes", rById.Name, "Expected to receive 'swarm-yes'.")
+	assert.Equal(t, "swarm-yes", rByName.Name, "Expected to receive 'swarm-yes'.")
+	assert.Equal(t, "", rFail.Name, "Expected to receive an empty result.")
+}
+
+func TestGetEncounterConditionValueList(t *testing.T) {
+	r := GetEncounterConditionValueList()
+	assert.Equal(t, "swarm-yes", r.Results[0].Name, "Expected to have a list of 'encounter condition' resource.")
+}

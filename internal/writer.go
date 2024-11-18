3467d277f5f0417aa5036df0a0c02f5602452860
Josh Guarino <jguarino722@gmail.com>
2024-11-18 17:14:21 -0500

    rename files

---
+++ internal/writer.go
@@ -0,0 +1,47 @@
+package internal
+
+import (
+	"encoding/json"
+	"encoding/xml"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+func WriteJSON(data interface{}, outputDir string, outputName string) {
+	file, err := os.Create(outputName + ".json")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", " ")
+	encoder.SetEscapeHTML(false)
+	encoder.Encode(data)
+}
+
+func WriteYAML(data interface{}, outputDir string, outputName string) {
+	file, err := os.Create(outputName + ".yaml")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	yaml, _ := yaml.Marshal(data)
+	file.Write(yaml)
+}
+
+func WriteXML(data interface{}, outputDir string, outputName string) {
+	file, err := os.Create(outputName + ".xml")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+}
+
+func WriteCSV(data interface{}, outputDir string, outputName string) {
+	file, err := os.Create(outputName + ".csv")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+}

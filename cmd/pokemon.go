57e5b1314405c644947850b623046078a1784ebe
Josh Guarino <jguarino722@gmail.com>
2024-12-19 10:52:35 -0500

    better naming convention

--- cmd/pokemon.go
+++ cmd/pokemon.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var resourceGroups = []string{
+var pokemonGroups = []string{
 	"Abilities", "Characteristics", "Egg Groups", "Genders", "Growth Rates",
 	"Natures", "Pokeathlon Stats", "Pokemon", "Pokemon Colors", "Pokemon Forms",
 	"Pokemon Habitats", "Pokemon Shapes", "Pokemon Species", "Stats", "Types",
@@ -24,25 +24,25 @@ var pokemonCmd = &cobra.Command{
 		data := internal.Data[interface{}]{}
 
 		// select prompt
-		prompt := internal.CreateListPrompt("Select pokemon resource group", resourceGroups)
-		resourceGroup := internal.RunSelectPrompt(prompt)
+		prompt := internal.CreateListPrompt("Select pokemon resource group", pokemonGroups)
+		pokemonGroup := internal.RunSelectPrompt(prompt)
 
 		// flag to search for specific resource else return paginated list
 		if search {
 			search := internal.RunSearchPrompt(internal.CreateSearchPrompt())
-			s, err := pokemon.GetSpecific(resourceGroup, search)
+			s, err := pokemon.GetSpecific(pokemonGroup, search)
 			if err != nil {
 				return
 			}
 			data.Data = s
 		} else {
-			p := pokemon.GetList(resourceGroup)
+			p := pokemon.GetList(pokemonGroup)
 			data.Data = p
 		}
 
 		// create file if output flag exists
 		if outputToFile {
-			internal.OutputToFile(data.Data, resourceGroup)
+			internal.OutputToFile(data.Data, pokemonGroup)
 		}
 		fmt.Println(data.Data)
 	},

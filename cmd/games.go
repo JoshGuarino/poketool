d35054331086f7ea7a84723fa4acc33613f19545
Josh Guarino <jguarino722@gmail.com>
2024-12-21 13:07:43 -0500

    refactor games cmd

--- cmd/games.go
+++ cmd/games.go
@@ -2,53 +2,50 @@ package cmd
 
 import (
 	"fmt"
-  "github.com/joshguarino/poketool/internal/games"
-  "github.com/manifoldco/promptui"
+
+	"github.com/joshguarino/poketool/internal"
+	"github.com/joshguarino/poketool/internal/games"
 	"github.com/spf13/cobra"
 )
+
+var gamesGroups = []string{"Generation", "Pokedex", "Version", "Version Group"}
 
 // gamesCmd represents the games command
 var gamesCmd = &cobra.Command{
 	Use:   "games",
 	Short: "Access pokemon resource group data from pokeapi: https://pokeapi.co/docs/v2#games-section",
 	Run: func(cmd *cobra.Command, args []string) {
-	// select prompt
-		prompt := promptui.Select{
-			Label: "Select games group resource",
-			Items: []string{"Generation", "Pokedex", "Version", "Version Group"},
-		}
-		_, result, err := prompt.Run()
-		if err != nil {
-			fmt.Printf("Prompt failed %v\n", err)
-			return
-		}
 
-		// flag to search for specific resource
+		// generic data holder struct
+		data := internal.Data[interface{}]{}
+
+		// select prompt
+		selectPrompt := internal.CreateListPrompt("Select games resource group", gamesGroups)
+		gamesGroup := internal.RunSelectPrompt(selectPrompt)
+
+		// flag to search for specific resource else return paginated list
 		if search {
-			// search prompt
-			prompt := promptui.Prompt{
-				Label: "Search",
-			}
-			search, err := prompt.Run()
-			if err != nil {
-				fmt.Printf("Prompt failed %v\n", err)
-				return
-			}
-
-			s, err := games.GetSpecific(result, search)
+			search := internal.RunSearchPrompt(internal.CreateSearchPrompt())
+			s, err := games.GetSpecific(gamesGroup, search)
 			if err != nil {
 				return
 			}
-			fmt.Println(s)
-			return
+			data.Data = s
+		} else {
+			g := games.GetList(gamesGroup)
+			data.Data = g
 		}
 
-		g := games.GetList(result)
-		fmt.Println(g)
+		// create file if output flag exists
+		if outputToFile {
+			internal.OutputToFile(data.Data, gamesGroup)
+		}
+		fmt.Println(data.Data)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(gamesCmd)
 	gamesCmd.Flags().BoolVarP(&search, "search", "s", false, "Find specific resource")
+	gamesCmd.Flags().BoolVarP(&outputToFile, "output", "o", false, "Output data to file")
 }

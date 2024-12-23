d8574feceb91e05f8552e06f1b2e0e292d3b7731
Josh Guarino <jguarino722@gmail.com>
2024-12-23 10:24:23 -0500

    refactor machines cmd

--- cmd/machines.go
+++ cmd/machines.go
@@ -3,52 +3,49 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/joshguarino/poketool/internal"
 	"github.com/joshguarino/poketool/internal/machines"
-	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
+
+var machinesGroups = []string{"Machine"}
 
 // machinesCmd represents the machines command
 var machinesCmd = &cobra.Command{
 	Use:   "machines",
 	Short: "Access pokemon resource group data from pokeapi: https://pokeapi.co/docs/v2#locations-section",
 	Run: func(cmd *cobra.Command, args []string) {
+
+		// generic data holder struct
+		data := internal.Data[interface{}]{}
+
 		// select prompt
-		prompt := promptui.Select{
-			Label: "Select machines group resource",
-			Items: []string{"Machine"},
-		}
-		_, result, err := prompt.Run()
-		if err != nil {
-			fmt.Printf("Prompt failed %v\n", err)
-			return
-		}
-		// flag to search for specific resource
+		selectPrompt := internal.CreateListPrompt("Select machines resource group", machinesGroups)
+		machinesGroup := internal.RunSelectPrompt(selectPrompt)
+
+		// flag to search for specific resource else return paginated list
 		if search {
-			// search prompt
-			prompt := promptui.Prompt{
-				Label: "Search",
-			}
-			search, err := prompt.Run()
-			if err != nil {
-				fmt.Printf("Prompt failed %v\n", err)
-				return
-			}
-
-			s, err := machines.GetSpecific(result, search)
+			search := internal.RunSearchPrompt(internal.CreateSearchPrompt())
+			s, err := machines.GetSpecific(machinesGroup, search)
 			if err != nil {
 				return
 			}
-			fmt.Println(s)
-			return
+			data.Data = s
+		} else {
+			l := machines.GetList(machinesGroup)
+			data.Data = l
 		}
 
-		m := machines.GetList(result)
-		fmt.Println(m)
+		// create file if output flag exists
+		if outputToFile {
+			internal.OutputToFile(data.Data, machinesGroup)
+		}
+		fmt.Println(data.Data)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(machinesCmd)
 	machinesCmd.Flags().BoolVarP(&search, "search", "s", false, "Find specific resource")
+	machinesCmd.Flags().BoolVarP(&outputToFile, "output", "o", false, "Output data to file")
 }

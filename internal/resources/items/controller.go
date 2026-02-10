313385a7af872f0c66a66fc8ec6005f4753b0697
Josh Guarino <jguarino722@gmail.com>
2026-02-10 14:35:33 -0500

    refactoring itmes command

--- internal/resources/items/controller.go
+++ internal/resources/items/controller.go
@@ -6,39 +6,45 @@ import (
 	itemsGroup "github.com/JoshGuarino/PokeGo/pkg/resources/items"
 )
 
-type Controller struct {
+type ItemsController struct {
 	items itemsGroup.Items
 }
 
-func (controller Controller) GetList(result string, limit int, offset int) (any, error) {
+func NewController() ItemsController {
+	return ItemsController{
+		items: itemsGroup.NewItemsGroup(),
+	}
+}
+
+func (c ItemsController) GetList(result string, limit int, offset int) (any, error) {
 	switch result {
 	case "Item":
-		return controller.items.GetItemList(limit, offset)
+		return c.items.GetItemList(limit, offset)
 	case "Item Attribute":
-		return controller.items.GetItemAttributeList(limit, offset)
+		return c.items.GetItemAttributeList(limit, offset)
 	case "Item Category":
-		return controller.items.GetItemCategoryList(limit, offset)
+		return c.items.GetItemCategoryList(limit, offset)
 	case "Item Fling Effect":
-		return controller.items.GetItemFlingEffectList(limit, offset)
+		return c.items.GetItemFlingEffectList(limit, offset)
 	case "Item Pocket":
-		return controller.items.GetItemPocketList(limit, offset)
+		return c.items.GetItemPocketList(limit, offset)
 	}
 
 	return nil, fmt.Errorf("Unable to find resource %s in group", result)
 }
 
-func (controller Controller) GetSpecific(result string, search string) (interface{}, error) {
+func (c ItemsController) GetSpecific(result string, search string) (any, error) {
 	switch result {
 	case "Item":
-		return controller.items.GetItem(search)
+		return c.items.GetItem(search)
 	case "Item Attribute":
-		return controller.items.GetItemAttribute(search)
+		return c.items.GetItemAttribute(search)
 	case "Item Category":
-		return controller.items.GetItemCategory(search)
+		return c.items.GetItemCategory(search)
 	case "Item Fling Effect":
-		return controller.items.GetItemFlingEffect(search)
+		return c.items.GetItemFlingEffect(search)
 	case "Item Pocket":
-		return controller.items.GetItemPocket(search)
+		return c.items.GetItemPocket(search)
 	}
 
 	return nil, fmt.Errorf("Unable to find resource %s in group", result)
--- internal/resources/items/controller_test.go
+++ internal/resources/items/controller_test.go
@@ -3,31 +3,31 @@ package items
 import (
 	"testing"
 
+	"github.com/JoshGuarino/PokeGo/pkg/models"
 	"github.com/joshguarino/poketool/internal"
-	"github.com/mtslzr/pokeapi-go/structs"
 	"github.com/stretchr/testify/assert"
 )
 
-var controller internal.IController = 
+var controller internal.IController = NewController()
 
 func TestGetList(t *testing.T) {
-	rItem := controller.GetList("Item")
-	rAttribute := controller.GetList("Item Attribute")
-	rCatergory := controller.GetList("Item Category")
-	rFling := controller.GetList("Item Fling Effect")
-	rPocket := controller.GetList("Item Pocket")
-	rFail := controller.GetList("test")
-	assert.IsType(t, []structs.Result{}, rItem.Results, "Expected to have array of type 'Result' struct.")
-	assert.NotEmpty(t, rItem.Results, "Expected to not have an empty array.")
-	assert.IsType(t, []structs.Result{}, rAttribute.Results, "Expected to have array of type 'Result' struct.")
-	assert.NotEmpty(t, rAttribute.Results, "Expected to not have an empty array.")
-	assert.IsType(t, []structs.Result{}, rCatergory.Results, "Expected to have array of type 'Result' struct.")
-	assert.NotEmpty(t, rCatergory.Results, "Expected to not have an empty array.")
-	assert.IsType(t, []structs.Result{}, rFling.Results, "Expected to have array of type 'Result' struct.")
-	assert.NotEmpty(t, rFling.Results, "Expected to not have an empty array.")
-	assert.IsType(t, []structs.Result{}, rPocket.Results, "Expected to have array of type 'Result' struct.")
-	assert.NotEmpty(t, rPocket.Results, "Expected to not have an empty array.")
-	assert.Equal(t, structs.Resource{}, rFail, "Expected to have empty struct of type Resource{}.")
+	rItem, _ := controller.GetList("Item", 20, 0)
+	rAttribute, _ := controller.GetList("Item Attribute", 20, 0)
+	rCatergory, _ := controller.GetList("Item Category", 20, 0)
+	rFling, _ := controller.GetList("Item Fling Effect", 20, 0)
+	rPocket, _ := controller.GetList("Item Pocket", 20, 0)
+	_, err := controller.GetList("test", 20, 0)
+	assert.IsType(t, &models.NamedResourceList{}, rItem, "Expected to have array of type 'NamedResourceList' struct.")
+	assert.NotEmpty(t, rItem, "Expected to not have an empty array.")
+	assert.IsType(t, &models.NamedResourceList{}, rAttribute, "Expected to have array of type 'NamedResourceList' struct.")
+	assert.NotEmpty(t, rAttribute, "Expected to not have an empty array.")
+	assert.IsType(t, &models.NamedResourceList{}, rCatergory, "Expected to have array of type 'Result' struct.")
+	assert.NotEmpty(t, rCatergory, "Expected to not have an empty array.")
+	assert.IsType(t, &models.NamedResourceList{}, rFling, "Expected to have array of type 'Result' struct.")
+	assert.NotEmpty(t, rFling, "Expected to not have an empty array.")
+	assert.IsType(t, &models.NamedResourceList{}, rPocket, "Expected to have array of type 'Result' struct.")
+	assert.NotEmpty(t, rPocket, "Expected to not have an empty array.")
+	assert.Error(t, err, "Expected an error to be thrown.")
 }
 
 func TestGetSpecific(t *testing.T) {
@@ -36,11 +36,11 @@ func TestGetSpecific(t *testing.T) {
 	rCatergory, _ := controller.GetSpecific("Item Category", "1")
 	rFling, _ := controller.GetSpecific("Item Fling Effect", "1")
 	rPocket, _ := controller.GetSpecific("Item Pocket", "1")
-	rFail, _ := controller.GetSpecific("test", "test")
-	assert.IsType(t, structs.Item{}, rItem, "Expected to have type 'Item' struct.")
-	assert.IsType(t, structs.ItemAttribute{}, rAttribute, "Expected to have type 'ItemAttribute' struct.")
-	assert.IsType(t, structs.ItemCategory{}, rCatergory, "Expected to have type 'ItemCategory' struct.")
-	assert.IsType(t, structs.ItemFlingEffect{}, rFling, "Expected to have type 'ItemFlingEffect' struct.")
-	assert.IsType(t, structs.ItemPocket{}, rPocket, "Expected to have type 'ItemPocket' struct.")
-	assert.Equal(t, nil, rFail, "Expected to have 'nil' value.")
+	_, err := controller.GetSpecific("test", "test")
+	assert.IsType(t, &models.Item{}, rItem, "Expected to have type 'Item' struct.")
+	assert.IsType(t, &models.ItemAttribute{}, rAttribute, "Expected to have type 'ItemAttribute' struct.")
+	assert.IsType(t, &models.ItemCategory{}, rCatergory, "Expected to have type 'ItemCategory' struct.")
+	assert.IsType(t, &models.ItemFlingEffect{}, rFling, "Expected to have type 'ItemFlingEffect' struct.")
+	assert.IsType(t, &models.ItemPocket{}, rPocket, "Expected to have type 'ItemPocket' struct.")
+	assert.Error(t, err, "Expected an error to be thrown.")
 }

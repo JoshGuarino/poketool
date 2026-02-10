ad60728067c792fe6df5730e1bd2249cb67356f0
Josh Guarino <jguarino722@gmail.com>
2026-02-09 20:18:00 -0500

    cleanup leftover file

--- internal/resources/contests/constants.go
+++
@@ -1,9 +0,0 @@
-package contests
-
-import "github.com/joshguarino/poketool/internal"
-
-const (
-	contestTypeEndpoint        = internal.PokeApiBaseUrl + "/contest-type"
-	contestEffectEndpoint      = internal.PokeApiBaseUrl + "/contest-effect"
-	superContestEffectEndpoint = internal.PokeApiBaseUrl + "/super-contest-effect"
-)
